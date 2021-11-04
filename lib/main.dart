b5391dc3a7dd3ae3af06304e20f476e913d77d5e
ismail shah <smily124000@gmail.com>
2021-11-05 01:36:25 +0500

    REMOVE COMMENTS

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-// <<<<<<< HEAD
+
 import 'view/main_screen.dart';
-// =======
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-// import 'view/list_of_images.dart';
-// >>>>>>> 6de00ebbda8e76f5426f8a887cebbb0de7b6fbe2
 
 void main() {
   runApp(Myapp());
@@ -17,30 +12,7 @@ class Myapp extends StatefulWidget {
 }
 
 class _MyappState extends State<Myapp> {
-  int counter = 0;
 
-// <<<<<<< HEAD
-// =======
-  @override
-  void initState() {
-    super.initState();
-    // fetchImages();
-  }
-
-  // void fetchImages() async {
-  //   counter++;
-  //   var response = await http
-  //       .get(Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
-  //   var url = json.decode(response.body);
-  //   print('URL: ' + url.toString());
-  //   var imageModel = new ImageModel.fromJson(url);
-  //   print('Image Model: ' + imageModel.toString());
-  //   setState(() {
-  //     images.add(imageModel);
-  //   });
-  // }
-
-// >>>>>>> 6de00ebbda8e76f5426f8a887cebbb0de7b6fbe2
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
